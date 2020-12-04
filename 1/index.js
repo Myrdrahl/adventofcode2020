15f932407cc001790c126fc4ad5aa8a8272e293b
Even Hauge Juberg <even.juberg@artsdatabanken.no>
2020-12-04 13:30:47 +0100

    Cleaning up

--- 1/index.js
+++ 1/index.js
@@ -1,17 +1,12 @@
 const fs = require('fs')
-const { parse } = require('path')
 const readline = require('readline')
 let expenses = []
 
 function checkFor2020(expenses) {
-    // console.log(expenses) array er kommet inn hit også!
     expenses.forEach(element => {
         let rest = (2020 - element)
-        //console.log(typeof(element))
-        //console.log(typeof(rest))
-        //console.log("Element: ", element)
-        //console.log("Rest: ", rest)
-        expenses.includes(rest) ? console.log("Winning: ",element ,rest) : console.log("Not the one")
+
+        expenses.includes(rest) ? console.log("Winning: ", (element * rest)) : null
         
     })
 }
@@ -25,13 +20,12 @@ async function processLines() {
         crlfDelay: Infinity
     })
     for await (let line of rl) {
-        //console.log(line)
+      
         line = parseInt(line)
         expenses.push(line)
     }
-    //console.log(expenses) array er komplett
+  
     checkFor2020(expenses)
 }
 
 processLines()
-
