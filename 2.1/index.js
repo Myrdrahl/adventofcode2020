2ab6b169793c499d53f5d4ca5aa1dbb732be6de6
Even Hauge Juberg <even.juberg@artsdatabanken.no>
2020-12-08 11:10:51 +0100

    sigh

--- 2.1/index.js
+++ 2.1/index.js
@@ -9,6 +9,9 @@ for (let i = 0; i < passwords.length; i++){
     firstPos = parseInt(highandlow[0])-1
     secondPos = parseInt(highandlow[1])-1
     letter = stuff[1].replace(':','')
+    //console.log(stuff[2])
+    //console.log(firstPos, stuff[2].charAt(firstPos))
+    //console.log(secondPos, stuff[2].charAt(secondPos))
     stuff[2].charAt(firstPos) === letter || stuff[2].charAt(secondPos) === letter ? valid++ : null
 }
 console.log(valid)
