4b2ed0531d93f7c17b4306abefa7abba1503533e
Even Hauge Juberg <even.juberg@artsdatabanken.no>
2020-12-08 10:53:38 +0100

    Hrmf

--- 2.1/index.js
+++ 2.1/index.js
@@ -3,12 +3,12 @@ let passwords = fs.readFileSync('passwords.txt').toString().split(/\n/)
 //console.log(passwords[999])
 let valid = 0
 //
-for (let i = 0; i < 1; i++){
+for (let i = 0; i < passwords.length; i++){
     let stuff = passwords[i].split(' ')
     let highandlow = stuff[0].split('-')
     highandlow[0] = parseInt(highandlow[0])+1
     highandlow[1] = parseInt(highandlow[1])+1
     stuff[1] = stuff[1].replace(':','')
-    stuff[2].charAt((highandlow[0])==stuff[1]) || stuff[2].charAt((highandlow[1]) == stuff[1]) ? valid++ : null
+    stuff[2].charAt((highandlow[0])) == stuff[1] || stuff[2].charAt((highandlow[1])) == stuff[1] ? valid++ : null
 }
 console.log(valid)
