7c5c3d47ceb7f077f775bc6b62a7d931a115b5db
Even Hauge Juberg <even.juberg@artsdatabanken.no>
2020-12-08 10:58:59 +0100

    Well, well, well.

--- 2.1/index.js
+++ 2.1/index.js
@@ -6,8 +6,8 @@ let valid = 0
 for (let i = 0; i < passwords.length; i++){
     let stuff = passwords[i].split(' ')
     let highandlow = stuff[0].split('-')
-    highandlow[0] = parseInt(highandlow[0])+1
-    highandlow[1] = parseInt(highandlow[1])+1
+    highandlow[0] = parseInt(highandlow[0])-1
+    highandlow[1] = parseInt(highandlow[1])-1
     stuff[1] = stuff[1].replace(':','')
     stuff[2].charAt(highandlow[0]) === stuff[1] || stuff[2].charAt(highandlow[1]) === stuff[1] ? valid++ : null
 }
